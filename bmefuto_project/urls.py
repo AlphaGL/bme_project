1e788e589329720ce55aa216d89eb92c1e5a5965
Ibeawuchi chukwugozirim <ibeawuchichukwugozirim@gmail.com>
2025-10-17 12:14:04 +0100

    url update

--- bmefuto_project/urls.py
+++ bmefuto_project/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('encrypted/admin/futobme', admin.site.urls),
+    path('encrypted/admin/', admin.site.urls),
     path('', include('core.urls')),
 ]
 
