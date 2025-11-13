ae0c3d3f437fad2582920676df60fc8f59ad8e81
Ibeawuchi chukwugozirim <ibeawuchichukwugozirim@gmail.com>
2025-11-13 03:16:47 +0100

    payment

--- bmefuto_project/settings.py
+++ bmefuto_project/settings.py
@@ -10,8 +10,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-your-secret-key-here')
 
-# DEBUG = config('DEBUG', default=True, cast=bool)
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
+# DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
